ffff4c771822b91d78ada18e0cb490df48f64168
gurbuzertunga <gurbuzertunga@gmail.com>
2021-04-20 17:48:25 +0300

    start redux store

---
+++ src/services/appointmentapi.js
@@ -0,0 +1,7 @@
+export const BASE = 'https://arcane-taiga-86251.herokuapp.com/';
+export const CONSULTANCIES = 'consultancies';
+export const APPOINTMENTS = 'appointments';
+export const SIGNUP = 'signup';
+export const LOGIN = 'auth/login';
+
+export default BASE;
