fbe984582a84e4942615c543b6bad03fef15804e
gurbuzertunga <gurbuzertunga@gmail.com>
2021-05-19 12:01:29 +0300

    update live version for redirect after refresh

--- src/services/appointmentapi.js
+++ src/services/appointmentapi.js
@@ -1,5 +1,5 @@
-export const BASE = 'https://aqueous-refuge-41349.herokuapp.com';
-// export const BASE = 'http://localhost:9000';
+// export const BASE = 'https://aqueous-refuge-41349.herokuapp.com';
+export const BASE = 'http://localhost:9000';
 export const CONSULTANCIES = '/consultancies';
 export const APPOINTMENTS = '/appointments';
 export const SIGNUP = '/signup';
