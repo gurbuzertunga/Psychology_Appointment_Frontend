709aa851ec8fe007513ed6c64445f3a4398d5873
gurbuzertunga <gurbuzertunga@gmail.com>
2021-05-24 18:45:20 +0300

    style error message

--- src/containers/Wrong.js
+++ src/containers/Wrong.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import wrongPass from '../assets/wrongpassword.jpg';
 
 const Wrong = () => (
-  <div>
-    <h1>Wrong Credentials</h1>
+  <div className="flex flex-col mx-auto">
+    <h1 className="text-center my-4 text-white text-2xl font-extrabold">Invalid Credentials</h1>
+    <img src={wrongPass} alt="Wrong Password" />
   </div>
 );
 
